4ad2e97592aaa7bc835280371e11e144357819a4
Kecreeper <Kecreeper@gmail.com>
2025-08-22 15:17:33 -0400

    unfunctional multithreading

--- main.py
+++ main.py
@@ -1,85 +1,38 @@
 from tkinter import *
 from tkinter import ttk
 from time import sleep
+import threading
 
 import clr
 clr.AddReference("LibreHardwareMonitorLib")
-from LibreHardwareMonitor import Hardware
+from LibreHardwareMonitor import Hardware # type: ignore
 
 class UpdateVisitor(Hardware.IVisitor):
     __namespace__ = "Stove"
     def VisitComputer(self, computer):
         computer.Traverse(self)
-
     def VisitHardware(self, hardware):
         hardware.Update()
         for subHardware in hardware.SubHardware:
             subHardware.Update()
-
     def VisitParameter(self, parameter):
         pass
-
     def VisitSensor(self, sensor):
         pass
 
 user = Hardware.Computer()
 user.IsCpuEnabled = True
 user.IsGpuEnabled = True
-user.IsBatteryEnabled = True
-user.IsMemoryEnabled = True
-user.IsNetworkEnabled = True
-user.IsStorageEnabled = True
+#user.IsBatteryEnabled = True
+#user.IsMemoryEnabled = True
+#user.IsNetworkEnabled = True
+#user.IsStorageEnabled = True
 user.Open()
 user.Accept(UpdateVisitor())
 
 array = {}
 def getVal(sensor: any, decimalPlaces: int=0):
     return str(round(sensor.Value, decimalPlaces))
-
-def updateArray() -> None:
-    for hw in user.Hardware:
-        hwType = str(hw.HardwareType).lower()
-        if "gpu" in hwType: hwType ="gpu"
-        array[hwType] = {"name": hw.Name}
-
-        for sensor in hw.Sensors:
-            sName = str(sensor.Name)
-            sType = str(sensor.SensorType)
-
-            #temp ----
-            if (sName == "Core Average") or \
-            (sName == "GPU Core" and sType == "Temperature"): 
-                array[hwType]["temp"] = getVal(sensor, 1)
-
-            #usagePercentage ----
-            elif (sName == "CPU Total") or \
-            (sName == "GPU Core" and sType == "Load") or \
-                (sName == "Memory Total"):
-                array[hwType]["usagePercentage"] = getVal(sensor, 1)
-
-            #memoryPercentage ----
-            elif (sName == "Gpu Core") or \
-            (sName == "Memory" and sType == "Load"):
-                array[hwType]["memoryPercentage"] = getVal(sensor, 1)
-            
-            #memorySpecific ----
-            elif (sName == "Memory Used"):
-                array[hwType]["memorySpecific"] = getVal(sensor, 1)
-            
-    #del array["gpu"]
-    if "gpu" not in array:
-        array["gpu"] = {
-            "name": "No GPU",
-            "temp": "N/A",
-            "usage": "N/A"
-        }
-
-updateArray()
-
-for hardware in user.Hardware:
-    print(f"Hardware: {hardware.Name}")
-    for sensor in hardware.Sensors:
-        print(f"\tSensor: {sensor.Name}, value: {sensor.Value}, TYPE: {sensor.SensorType}")
 
 main = Tk()
 main.title("Stove")
@@ -90,24 +43,72 @@ frame.grid(column=0, row=0)
 
 cpuName = StringVar()
 ttk.Label(frame, textvariable=cpuName).grid(column=1, row=0)
-cpuName.set(array["cpu"]["usagePercentage"])
 
 gpuName = StringVar()
 ttk.Label(frame, textvariable=gpuName).grid(column=2, row=0)
-gpuName.set(array["gpu"]["usagePercentage"])
 
 for x in frame.winfo_children():
     x['padding'] = 10
 
-def upd():
-    print("a")
-    updateArray()
-    user.Open()
-    user.Accept(UpdateVisitor())
-    cpuName.set(array["cpu"]["usagePercentage"])
-    gpuName.set(array["gpu"]["usagePercentage"])
-    main.update()
-    main.after(1000, upd)
+def updateSensors():
+    while True:
+        user.Open()
+        user.Accept(UpdateVisitor())
+        sleep(1)
 
-main.after(1000, upd)
+t = threading.Thread(target=updateSensors)
+l = threading.Lock()
+t.start()
+
+def updateArray() -> None:
+    with l:
+        for hw in user.Hardware:
+            hwType = str(hw.HardwareType).lower()
+            if "gpu" in hwType: hwType ="gpu"
+            array[hwType] = {"name": hw.Name}
+
+            for sensor in hw.Sensors:
+                sName = str(sensor.Name)
+                sType = str(sensor.SensorType)
+
+                #temp ----
+                if (sName == "Core Average") or \
+                (sName == "GPU Core" and sType == "Temperature"): 
+                    array[hwType]["temp"] = getVal(sensor, 1)
+
+                #usagePercentage ----
+                elif (sName == "CPU Total") or \
+                (sName == "GPU Core" and sType == "Load") or \
+                (sName == "Memory Total"):
+                    array[hwType]["usagePercentage"] = getVal(sensor, 1)
+                    print("percent: " + array[hwType]["usagePercentage"] + " | " + sType +sName)
+
+                #memoryPercentage ----
+                elif (sName == "Gpu Core") or \
+                (sName == "Memory" and sType == "Load"):
+                    array[hwType]["memoryPercentage"] = getVal(sensor, 1)
+                
+                #memorySpecific ----
+                elif (sName == "Memory Used"):
+                    array[hwType]["memorySpecific"] = getVal(sensor, 1)
+                
+        #del array["gpu"]
+        if "gpu" not in array:
+            array["gpu"] = {
+                "name": "No GPU",
+                "temp": "N/A",
+                "usage": "N/A"
+            }
+
+updateArray()
+
+def refresh():
+    updateArray()
+    with l:
+        cpuName.set(array["cpu"]["usagePercentage"])
+        gpuName.set(array["gpu"]["usagePercentage"])
+        main.update()
+        main.after(1000, refresh)
+
+main.after(1000, refresh)
 main.mainloop()
